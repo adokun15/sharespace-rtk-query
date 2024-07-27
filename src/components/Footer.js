c37df116605793d5303fb193c05cf4a958ff230f
adokun15 <amosdaniel2005@gmail.com>
2024-07-27 15:14:50 +0100

    Blog Page Route added

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-descriptive_color text-center py-10 bg-main_color">
+      &copy;ShareSpace<span>2024</span>
+    </footer>
+  );
+}
