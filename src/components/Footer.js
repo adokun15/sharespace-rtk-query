242cebf7f173e2d11e4ca3f690fa1a658acf33c0
adokun15 <amosdaniel2005@gmail.com>
2024-07-27 19:30:32 +0100

    Newsletter Page Route Completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-descriptive_color text-center py-10 bg-main_color">
+    <footer className=" text-descriptive_color text-center py-10 bg-main_color">
       &copy;ShareSpace<span>2024</span>
     </footer>
   );
