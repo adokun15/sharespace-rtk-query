dfbdf22605742890b9ecfe386333bd6e10794f05
Ohida <amosdaniel2005@gmail.com>
2025-11-02 03:17:25 +0100

    changed firebase authdomian

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,7 +12,7 @@ export const FEEDBACK_URL = "https://forms.gle/2QnuRvNrDYG6aogm7";
 export const SUPPORT_EMAIL = "mailto:contactdanielamos@gmail.com";
 
 export const FIREBASE_APIKEY = "AIzaSyDsU2heMX_fy89fl1WjvjHel6O1lFyeuAk";
-export const FIREBASE_AUTH_DOMAIN = "sharespace.com.ng";
+export const FIREBASE_AUTH_DOMAIN = "www.sharespace.com.ng";
 export const FIREBASE_PROJECT_ID = "sharespaceng";
 export const FIREBASE_STORAGE_BUCKET = "sharespaceng.appspot.com";
 export const FIREBASE_MESSAGING_SENDER_ID = "579110495977";
